697c113757ebc1084e97deb201d9e945f05cbb82
mr_godsplanELot <125884479+GodsplanElot@users.noreply.github.com>
2024-10-04 10:56:20 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 /**
-* Template Name: iPortfolio
-* Updated: Jan 29 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+⚡
 */
 (function() {
   "use strict";
